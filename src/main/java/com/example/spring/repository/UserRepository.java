5440dfe0414038c60f56111c55b96d93e928985b
focomoko <focomoko@mail.ru>
2026-04-16 20:17:28 +0500

    add repository

---
+++ src/main/java/com/example/spring/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.spring.repository;
+
+import com.example.spring.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,Long> {
+
+}
