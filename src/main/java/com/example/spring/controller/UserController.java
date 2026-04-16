4cb722312b6387a499adfa7cfd5fcb77c0d017f4
focomoko <focomoko@mail.ru>
2026-04-16 20:52:17 +0500

    add controller end-point

--- src/main/java/com/example/spring/controller/UserController.java
+++ src/main/java/com/example/spring/controller/UserController.java
@@ -1,11 +1,24 @@
 package com.example.spring.controller;
 
+import com.example.spring.DTO.UserRequestDTO;
+import com.example.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    
+    private UserService userService;
 
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @PostMapping("/save")
+    public void save(@RequestBody UserRequestDTO userRequestDTO) {
+        userService.save(userRequestDTO);
+    }
 }
